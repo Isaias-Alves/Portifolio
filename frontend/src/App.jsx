1b29a7979f695e9a33b44fc17c4d145f921ba78f
Isaias-Alves <isaias.alves@sga.pucminas.br>
2026-02-12 23:22:52 -0300

    feat: função de Scroll

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,16 +7,13 @@ import dharmaDark from "./assets/dharma-white.png";
 const App = () => {
 
     const [theme, setTheme] = useState("dark");
-    const [rotation, setRotation] = useState(0);
+    const [scrollY, setScrollY] = useState(0);
     const [image, setImage] = useState(dharma);
 
-    useEffect(() => { 
-        const handleScroll = () => {
-            setRotation(window.scrollY / 5)
-        }
-        window.addEventListener("scroll", handleScroll);
-        return window.removeEventListener("scroll", handleScroll);
-    }, [])
+    const handleScroll = (e) => {
+    const position = e.currentTarget.scrollTop; 
+    setScrollY(position);
+  }
 
     useEffect(() => {
         if (theme === "dark") {
@@ -31,8 +28,10 @@ const App = () => {
     }
 
     return (
-        <div className="min-h-[300vh] bg-amber-50 text-neutral-900 dark:bg-neutral-950 dark:text-white transition-colors duration-200 relative">
-
+        <div
+        onScroll={handleScroll} 
+        className="h-screen bg-amber-50 text-neutral-900 dark:bg-neutral-950 dark:text-white transition-colors duration-200 relative overflow-y-auto">
+            
             <button 
             onClick={toggleTheme}
             className="fixed top-6 right-6 z-50 p-3 rounded-full bg-neutral-300 dark:bg-neutral -900 hover:scale-110 transition-transform"
@@ -44,12 +43,20 @@ const App = () => {
                 <img src={theme === "dark" ? dharma : dharmaDark} 
                 alt="Dharma" 
                 className="w-[80vw] md:w-160 opacity-20 transition-transform duration-500 ease-out" 
-                style={{ transform: `rotate(${rotation}deg)` }}
+                style={{ transform: `rotate(${scrollY / 5}deg)` }}
                 />
             </div>
+            <div className="relative z-10 container mx-auto px-4 pt-[40vh] pb-[20vh] text-center">
+        
+        <h1 className="text-5xl md:text-7xl font-bold mb-6 tracking-tighter">
+          DEV <span className="text-indigo-500">MAHORAGA</span>
+        </h1>
+        
+        <p className="text-xl mb-32 max-w-2xl mx-auto opacity-70">
+          "Role para ver a adaptação acontecer."
+        </p>
 
-            
-
+      </div>
 
         </div>
     )
