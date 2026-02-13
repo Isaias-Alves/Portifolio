8d03621b648345d5f784426ddb9f1c20ec46aa80
Isaias-Alves <isaias.alves@sga.pucminas.br>
2026-02-12 22:38:26 -0300

    feat: listener para escutar o scroll criado

--- frontend/src/assets/main.jsx
+++ frontend/src/assets/main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { use } from "react";
 import { useState, useEffect } from "react";
 import dharma from "./dharma-white.jpg";
 import dharmaDark from "./dharma-black.jpg";
@@ -6,6 +6,15 @@ import dharmaDark from "./dharma-black.jpg";
 
 const App = () => {
 
-    const [darkMode, setDarkMode] = useState(false);
+    const [theme, setTheme] = useState("dark");
+    const [rotation, setRotation] = useState(0);
+    const [image, setImage] = useState(dharma);
 
+    useEffect(() => { 
+        const handleScroll = () => {
+            setRotation(window.scrollY / 5)
+        }
+        window.addEventListener("scroll", handleScroll);
+        return window.removeEventListener("scroll", handleScroll);
+    }, [])
  }
