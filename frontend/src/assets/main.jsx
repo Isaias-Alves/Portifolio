6b08e71b458bcd3f5a7d426df7b221914e5a598b
Isaias-Alves <isaias.alves@sga.pucminas.br>
2026-02-12 22:35:22 -0300

    feat: estruturação da pasta de trabalho inicial e começo do codigo main

---
+++ frontend/src/assets/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import dharma from "./dharma-white.jpg";
+import dharmaDark from "./dharma-black.jpg";
+
+
+const App = () => {
+
+    const [darkMode, setDarkMode] = useState(false);
+
+ }
